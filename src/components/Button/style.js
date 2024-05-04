cd76cd43d4be2905a4e5623d3aa6dd3cf9b8c5e8
lovisaviigisalu <122033147+lovisaviigisalu@users.noreply.github.com>
2024-05-04 21:01:51 +0300

    Create color management file

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,16 +1,17 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
         marginTop:30,
-        backgroundColor: "#4F63AC",
+        backgroundColor: colors.orange,
         paddingHorizontal: 8,
         paddingVertical: 20,
         borderRadius: 8,
         width:'100%'
     },
     title:{
-        color: '#ffffff',
+        color: colors.white,
         textAlign: 'center',
         fontSize: 16,
         fontWeight: 'bold'
